f220396bf35ded6a7ce934ae5babe03b25b62e6c
Moa <moa.lovas@gmail.com>
2024-04-08 11:39:02 +0200

    Set up Ovveride fun, User setup.

--- app/src/main/java/com/katja/splashmessenger/User.kt
+++ app/src/main/java/com/katja/splashmessenger/User.kt
@@ -3,7 +3,7 @@ package com.katja.splashmessenger
 class User(val id: String, val fullName: String, val email: String, val password: String) {
 
     override fun toString(): String {
-        return "User('id = $id', )"
+        return "User('id = $id', 'fullName = $fullName', 'email = $email', 'password = $password')"
     }
 }
 

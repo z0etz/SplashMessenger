5de7b3d8cee12469271b79b7cb755f3dbae31d62
Moa <moa.lovas@gmail.com>
2024-04-24 14:16:39 +0200

    Cleaned Messege and MessegeAdapter, with Katja.

--- app/src/main/java/com/katja/splashmessenger/Message.kt
+++ app/src/main/java/com/katja/splashmessenger/Message.kt
@@ -1,8 +1,5 @@
 package com.katja.splashmessenger
 
-import java.time.LocalDate
-import java.time.LocalDateTime
-
 data class Message(val id: String?, val conversationId: String?, val senderId: String?,
                    val type:  MessageType?, val text: String?, val timestamp: Long)
 
